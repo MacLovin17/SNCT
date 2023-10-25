f814df5aa057b1a1f8662be5babf940eefde6be5
Erik Gomes <142506909+MacLovin17@users.noreply.github.com>
2023-10-25 20:10:41 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+# include <locale.h>
+
+typedef struct{
+    int id;
+    char nome [10];
+}dados;
 
 int main(){
-    printf("Carlos é gay");
+    dados da[2];
+ int i;
+    
+for( i= 0;i<2;i++ )
+{
+    scanf("%s", &da[i].nome);
+        fflush;
+        scanf("%d",&da[i].id);
+}
+    for(int i = 0 ; i<2 ; i++){
+
+    printf("Nome : %s \n idade : %d \n",da[i].nome, da[i].id );
+    }
+
+    setlocale(LC_ALL, "");
     return 0;
 }
